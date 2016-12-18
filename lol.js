c573577d339ce71cf8bab141e653e76a25719da4
podopie <epodojil@gmail.com>
2016-12-18 11:59:35 -0500

    add favicon and some other things

--- lol.js
+++ lol.js
@@ -19,7 +19,7 @@ app.use('/zen', express.static(__dirname + '/node_modules/zenscroll/'))
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
